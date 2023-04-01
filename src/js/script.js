fe36cedc084151f72c2a4da9e2b3174cef64572b
Ilyas <ilyas.shamshiden@mail.ru>
2023-04-01 10:46:07 +0600

    almost done

--- src/js/script.js
+++ src/js/script.js
@@ -1,103 +1,124 @@
+
 $(document).ready(function(){
-    $('.slider').slick({
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/3/left-removebg-preview.png" alt="#"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/3/right-removebg-preview.png" alt="#"></button>',
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    arrows: false,
-                    dots: true,
-                    autoplay: true,
-                }
-            }
-        ]
-    });
+  $('.slider').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/3/left-removebg-preview.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/icons/3/right-removebg-preview.png"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false
+              }
+          }
+      ]
+  });
+  
+  $('ul.catalog__caption').on('click', 'li:not(.catalog__caption__list_active)', function() {
+      $(this)
+        .addClass('catalog__caption__list_active').siblings().removeClass('catalog__caption__list_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
 
-    $('ul.catalog__caption').on('click', 'li:not(catalog__caption__list_active)', function() {
-        $(this)
-          .addClass('catalog__caption__list_active').siblings().removeClass('catalog__caption__list_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
-    function toggleSlide (item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__content_back').eq(i).toggleClass('catalog-item__content_back_active');
-            });
-        });
-    };
-    toggleSlide ('.catalog-item__content__link');
-    toggleSlide ('.catalog-item__content__link_back');
+  function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__content_back').eq(i).toggleClass('catalog-item__content_back_active');
+          })
+      });
+  };
 
-    //modal
+  toggleSlide('.catalog-item__content__link');
+  toggleSlide('.catalog-item__content__link_back');
 
-    $('.modal__close').on('click', function(){
-        $('.overlay').fadeOut();
-    });
+  // Modal
 
-    $('.button_mini').each(function(i){
-        $(this).on('click', function(){
-            $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn();
-        });
-    });
+  $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
 
-    $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn();
-    });
+  $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+          $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+      })
+  });
 
-    function valideForms (form) {
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
+  function validateForms(form){
+      $(form).validate({
+          rules: {
+              name: {
+                  required: true,
+                  minlength: 2
+              },
+              phone: "required",
+              email: {
+                  required: true,
+                  email: true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символа!")
                 },
-                phone: {
-                    required: true,
-                }
-            },
-            messages: {
-                name: {
-                    required: "Введите ваше имя",
-                    minlength: jQuery.validator.format("Пожалуйста, введите хотя бы {0} буквы")
-                },
-                phone: {
-                    required: "Введите ваш номер телефона",
-                }
-            }
-        });
-    }
+              phone: "Пожалуйста, введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              }
+          }
+      });
+  };
 
-    valideForms ('#consultation-form');
-    valideForms ('#consultation form');
-    valideForms('#order form');
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 
-    $('input[name=phone]').mask("+7(999) 999-9999");
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    $('form').submit(function(e) {
+  $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
 
-        if (!$(this).valid()){ 
-            return; 
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+//   Smooth scroll and pageup
+
+    var tempScrollTop = 0;
+    $(window).scroll(function() {
+        var currentScrollTop = $(this).scrollTop();
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
         }
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-            $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
-
-            $('form').trigger('reset');
-        });
-        return false;
+        tempScrollTop = currentScrollTop;
     });
+
+  $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
+  //wow
+    new WOW().init();
 });
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,8 +13,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'shamshide2005gmail.com';                 // Наш логин
-$mail->Password = 'Almaty2005';                           // Наш пароль от ящика
+$mail->Username = 'shamshiden2005@gmail.com';                 // Наш логин
+$mail->Password = 'tuwshufbgyhodyrc';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
